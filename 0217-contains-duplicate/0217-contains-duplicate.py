e993c07808efa0aaef72c813195d71819b69a63d
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-01-25 17:13:21 -0600

    Time: 11 ms (53.95%), Space: 31.5 MB (44.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,15 @@ class Solution:
         Brute Force: For each num in nums, loop through the rest of the list to see if that number
         appears again. If you make it through the whole list for each number, then there are no
         duplicates.
-            Time: O(n)
+            Time: O(n^2)
             Space: O(1)
 
         Optimized: Use a hashset with O(1) lookup/existence. Keep track of the ones seen so far.
         If you encounter one that has been seen already, there is a duplicate so return True.
         If you make it through the entire list without seeing a duplicate in seen, there is 
         no duplicate so return False.
+            Time: O(n)
+            Space: O(n)
         """
 
         seen = set()
