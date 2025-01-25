5427ca8790c3089ed15ac35782290b75c6d5d9a8
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-01-25 17:44:42 -0600

    Time: 0 ms (100%), Space: 18.8 MB (36.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,27 @@
 class Solution:
+    # Optimized
+    # Time: O(n)
+    # Space: O(n)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = set()
+        """
+        The whole algorithm revolves around complements. Loop through nums. Use a hashmap to
+        store the current number and its index. If we find the complement in the hashmap,
+        then we have our solution. A key distinction to make is adding to the hashmap only
+        AFTER we check if the complement is in the hashmap. This is done to prevent the use
+        of the same number in adding up to the target.
+        """
         num_index = {}
         for i, num in enumerate(nums):
             complement = target - num
-            if complement in seen:
+            if complement in num_index:  # this is actually an O(1) operation!
                 return [i, num_index[complement]]
             else:
                 num_index[num] = i
-                seen.add(num)
 
     """
-    # Brute Force
-    # Time: O(n)
-    # Space: O(1)
+    Brute Force
+    Time: O(n)
+    Space: O(1)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
