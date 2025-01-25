7e7d2c02f7a0b010708d7b8571c3866d84cbb7c7
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-01-25 17:10:21 -0600

    Time: 4 ms (90.96%), Space: 31.6 MB (44.66%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        Brute Force: For each num in nums, loop through the rest of the list to see if that number
+        appears again. If you make it through the whole list for each number, then there are no
+        duplicates.
+            Time: O(n)
+            Space: O(1)
+
+        Optimized: Use a hashset with O(1) lookup/existence. Keep track of the ones seen so far.
+        If you encounter one that has been seen already, there is a duplicate so return True.
+        If you make it through the entire list without seeing a duplicate in seen, there is 
+        no duplicate so return False.
+        """
+
+        seen = set()
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+            else:
+                return True
+        return False
