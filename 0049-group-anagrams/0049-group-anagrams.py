1a87dd0d4329007c2a03feb92c4777bf24aaa318
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-01-25 18:18:47 -0600

    Time: 7 ms (98.19%), Space: 20.7 MB (67.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 
 
 class Solution:
+    # Time: O(n mlog(m)) where n is the length of strs and m is the length of the longest string
+    # in strs
+    # Space: O(n) since all strings will go in the hashmap by the end of the algorithm
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         sorted_group = defaultdict(list)
         for s in strs:
@@ -13,4 +16,3 @@ class Solution:
             out.append(sorted_group[key])
         
         return out
-    
