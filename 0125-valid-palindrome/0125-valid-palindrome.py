54978bd46b642b0cc0317de5b8a5defc34983a39
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-07-12 19:35:58 -0500

    Time: 7 ms (81.1%), Space: 18.1 MB (56.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,38 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         """
-        Time: O(n)
-        Space: O(1)
-        """
-        if len(s) == 1:
-            return True
+        ### Converting the given palindrome check to code ###
+        # Time: O(n)
+        # Space: O(n), space needed for new string
 
+        # 1. Standardize string to lowercase
+        s = s.lower().strip()
+
+        # 2. Remove all non alphanumeric characters
+        new_s = ""
+        for char in s:
+            if char.isalnum():
+                new_s += char
+        
+        # 3. Does the string read the same forwards and backwards?
+        return new_s == new_s[::-1]
+        """
+
+        # Two pointers extreme: One at left end (L), one at right end (R)
         L = 0
         R = len(s) - 1
-
         while L < R:
+            # Move pointers to valid alpha-numeric characters
             while not s[L].isalnum() and L < R:
                 L += 1
-            while not s[R].isalnum() and L < R:
+            while not s[R].isalnum() and R > L:
                 R -= 1
             
-            # At this point, we are on valid characters
+            # Now that both pointers are at valid chars, compare the chars
             if s[L].lower() != s[R].lower():
-                return False
+                return False  # not equal means the string is not a palindrome
+            
+            # Move pointers closer to progress
             L += 1
             R -= 1
         
