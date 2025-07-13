3a80c6288175f5dde7a2968620077800798d251d
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-07-12 20:01:05 -0500

    Time: 3 ms (81.02%), Space: 18.6 MB (59.39%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,33 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         """
-        Time: O(n)
-        Space: O(1)
+        # Brute Force: Check every possible sum combination to see
+        # if any lead to the target sum
+        # Time: O(n^2)
+        # Space: O(1)
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return (i+1, j+1)
+        return (-1, -1)
         """
+
+        # Opposite Ends Pointers
+        # The fact that numbers is sorted means that...
+            # incrementing the L pointer yields a larger sum
+            # decrementing the R pointer yields a smaller sum
+        # Time: O(n)  -> Pointers either break early or meet in middle, never crossing
+        # Space: O(1) -> Only 2 integer variables regardless of length of numbers
         L = 0
         R = len(numbers) - 1
-        while L < R:
-            summ = numbers[L] + numbers[R]
-            
-            # the sum is too big, make it smaller
-            if summ > target:
-                R -= 1
 
-            # the sum is too small, make it bigger
-            elif summ < target:
+        while L < R:
+            curr_sum = numbers[L] + numbers[R]
+            if curr_sum == target:
+                return (L+1, R+1)
+            if curr_sum < target:
                 L += 1
-            
-            # found the two numbers for the sum, return the indices (1-based)
-            else:
-                return [L+1, R+1]
-        
-        # not found
-        return [-1, -1]
-        
+                continue
+            if curr_sum > target:
+                R -= 1
+        return (-1, -1)  # should not happen
