31d84a3a9410fac8ff3dc8b6419742ca7bea77af
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-08-16 14:58:25 -0500

    Time: 0 ms (100%), Space: 18.2 MB (31.03%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,28 +1,33 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         """
-        Rotating an array 1 time means moving each number to the right by 1 position. Rotating an
-        array k times means shifting over each number to the right k times (with wrap around)
+        - `nums` is sorted in ascending order
+        - `nums` may be rotated between [1, n] times
+        - Rotating wraps elements around to the beginning
 
-        Looking at the testcase, we can notice a pattern of comparing the middle element to the
-        rightmost element.
-            • if nums[middle] > nums[right]: look in the right half
-            • else (nums[middle] < nums[right]): look in the left half
-            • don't need to worry about nums[middle] == nums[right] since "all the integers of nums
-            are unique"
+        ### WHY BINARY SEARCH ###
+        We know that nums after the rotations still has some sort of sorted order
+        and it started off sorted. This is a hint to use BINARY SEARCH.
         """
+        L = 0              # inclusive
+        R = len(nums) - 1  # inclusive
 
-        minn = float("inf")
-        left = 0               # inclusive
-        right = len(nums) - 1  # inclusive
+        while L < R:
+            mid = (L + R) // 2
 
-        while left <= right:
-            middle = (left + right) // 2
-            minn = min(minn, nums[middle])
-            if nums[middle] > nums[right]:
-                left = middle + 1
-            # else works since "all the integers of nums are unique"
+            # [3,4,5,1,2]
+            # Ex. 1 -> nums[mid]=5   nums[R]=2
+            if nums[mid] > nums[R]:
+                # Pivot in the right half of nums, but WHY?
+                # nums is sorted in ascending order. This means if
+                # nums[mid] > nums[R], there must be some numbers in between those.
+                # Those numbers contain the min.
+                L = mid + 1
+            
+            # Ex. 2 -> nums[L]=7    nums[mid]=13
             else:
-                right = middle - 1
+                # Pivot is either nums[mid] or in the left half of nums
+                # [7, 9, 1, 4, 5]
+                R = mid
 
-        return minn
+        return nums[L]
