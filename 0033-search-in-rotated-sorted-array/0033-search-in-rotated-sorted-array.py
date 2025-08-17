be3128fd19ded2d635cb8a71d3385eb3234c7be0
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2025-08-16 19:48:19 -0500

    Time: 0 ms (100%), Space: 18 MB (91.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,45 +4,31 @@ class Solution:
         * nums is sorted in ascending order (all with distinct values)
         * target may or MAY NOT be in nums (return -1 if its not there)
             otherwise return the index of target
-        
-        When you split the array down the pivot (where the nums switch) ascension,
-        both halves will be sorted.
-
-        There are 3 indices of interest:
-            * 0: the start of the array
-            * len(nums)-1: the end of the array
-            * mindex: the index of the minimum in the array that splits the
-                array into 2 sorted halves
         """
         L = 0
         R = len(nums)-1
-        while L < R:
-            mid = (L + R) // 2
-            if nums[mid] > nums[R]:
-                L = mid + 1
-            else:
-                R = mid
 
-        ### Binary Search in Sorted Part ###
-        mindex = L
-        if target < nums[mindex]:
-            return -1
-        if target == nums[mindex]:
-            return mindex
-        if nums[mindex] < target <= nums[-1]:
-            L = mindex + 1
-            R = len(nums)
-        else:
-            L = 0
-            R = mindex
-
-        while L < R:
+        while L <= R:
             mid = (L + R) // 2
-            if nums[mid] < target:    # we are too small
-                L = mid + 1           # make it bigger
-            elif nums[mid] > target:  # we are too big
-                R = mid               # make it smaller
-            else:
+            if nums[mid] == target:
                 return mid
-        
+
+            # Left half is sorted
+            if nums[L] <= nums[mid]:
+                # target is in the left half
+                if nums[L] <= target < nums[mid]:
+                    R = mid - 1
+                # target is in the right half
+                else:
+                    L = mid + 1
+            
+            # Right half is sorted
+            else:
+                # target is in the left half
+                if nums[mid] < target <= nums[R]:
+                    L = mid + 1
+                # target is in the right half
+                else:
+                    R = mid - 1
+
         return -1
