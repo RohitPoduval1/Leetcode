cc5f42a0661e5e91f5c889ca158cc1c4954a0e09
Rohit Poduval <77207985+RohitPoduval1@users.noreply.github.com>
2026-02-07 20:27:40 -0600

    Time: 20 ms (37.37%), Space: 22.8 MB (35.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var seen = Set<Int>()
+
+        for num in nums {
+            if seen.contains(num) {
+                return true
+            }
+            seen.insert(num)
+        }
+        
+        return false
+    }
+}
